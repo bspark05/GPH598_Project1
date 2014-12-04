a3230abdb291c996f4caa1a73e031a4cdc4c169b
bspark05 <bpark21@asu.edu>
2014-12-05 08:15:48 +0900

    12052014-1

--- Polygon.py
+++ Polygon.py
@@ -1,5 +1,5 @@
-
 from scipy.spatial.qhull import ConvexHull
+import numpy as np
  
 
 def __init__(self, params):
@@ -11,12 +11,12 @@ def makeConvexHull(points, incremental=False, qhull_options=None):
     hull = ConvexHull(points, incremental=False, qhull_options=None)
     convexHullList=[]
     for j in hull.vertices:
-        convexHullList.append([points[j,0], points[j,1]])
+        convexHullList.append([points[j][0], points[j][1]])
     convexHullArray = np.array(convexHullList)
     return convexHullArray
     
- def makeMBR(points):
+def makeMBR(points):
     min_x, min_y = np.min(points, axis=0)
     max_x, max_y = np.max(points, axis=0)
-    mbrArray = np.array([(min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, min_y)])
+    mbrArray = np.array([(min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, max_y)])
     return mbrArray
